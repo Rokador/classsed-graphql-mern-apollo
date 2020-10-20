cf979343b79920d40c9e9cacbe55ab7fbc489dcf
Rokador <rokador@live.com.my>
2020-10-20 21:27:20 +0800

    users.js might have a bug. not sure need to check

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
 mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
