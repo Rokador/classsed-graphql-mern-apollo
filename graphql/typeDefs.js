8e81a9c3eaa97987c40c7c553e0a5c6443e3d022
Rokador <rokador@live.com.my>
2020-10-29 16:57:47 +0800

    bug was fixed

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,12 +7,6 @@ module.exports = gql`
     createdAt: String!
     username: String!
   }
-  input RegisterInput {
-      username: String!
-      password: String!
-      confirmedPassword: String!
-      email: String!
-  }
   type User {
       id: ID!
       email: String!
@@ -20,6 +14,13 @@ module.exports = gql`
       username: String!
       createdAt: String!
   }
+  input RegisterInput {
+      username: String!
+      password: String!
+      confirmedPassword: String!
+      email: String!
+  }
+
   type Query {
     getPosts: [Post]
   }
