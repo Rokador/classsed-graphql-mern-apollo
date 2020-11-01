1233e877d914317863886825a9887cd0010bd2bc
Rokador <rokador@live.com.my>
2020-11-01 10:48:08 +0800

    asynIterator issue not fixed. added client folder (create-react-app)

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError, PubSub, UserInputError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
@@ -29,7 +29,10 @@ module.exports = {
     Mutation: {
         async createPost(_, { body }, context) {
             const user = checkAuth(context);
-            console.log(user);
+
+            if (arguments.body.trim() === '') {
+                throw new Error('Post body must not be empty');
+            }
 
             const newPost = new Post({
                 body,
