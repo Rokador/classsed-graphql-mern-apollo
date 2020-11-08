24672e73954ba510eebb6f7fa1513e338caca75a
Rokador <rokador@live.com.my>
2020-11-08 16:15:01 +0800

    added MyPopup

--- client/src/components/LikeButton.js
+++ client/src/components/LikeButton.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import { Button, Label, Icon } from 'semantic-ui-react'
+import MyPopup from '../util/MyPopup'
 
 function LikeButton({ user, post: { id, likes, likeCount } }) {
     const [liked, setLiked] = useState(false)
@@ -34,7 +35,9 @@ function LikeButton({ user, post: { id, likes, likeCount } }) {
 
     return (
         <Button as="div" labelPosition="right" onClick={likePost}>
-            {likeButton}
+            <MyPopup content={liked ? 'Unlike' : 'Like'}>
+                {likeButton}
+            </MyPopup>
             <Label basic color="teal" pointing="left">
                 {likeCount}
             </Label>
